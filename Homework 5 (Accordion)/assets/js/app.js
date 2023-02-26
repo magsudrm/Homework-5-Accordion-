deb819d07c9e31df11024cda0dad611c9f71ea41
unknown <magsudrm@code.edu.az>
2023-02-26 22:12:51 +0400

    project ended

--- Homework 5 (Accordion)/assets/js/app.js
+++ Homework 5 (Accordion)/assets/js/app.js
@@ -28,3 +28,57 @@ function removeOpen(index1) {
   });
 }
 
+
+
+
+
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  const result = [];
+  
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+
+function findElementsInString(str, arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+const str = "En yaxsi tapsiriqlari Maqsud yazir . Etiraz eden varsa cixsin bura";
+const arr = ["yaxsi", "Maqsud", "Etiraz", "varsa", "bura"];
+const result = findElementsInString(str, arr);
+console.log(result); 
+
+
+
+
